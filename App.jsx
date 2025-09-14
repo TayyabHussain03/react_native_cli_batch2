89dd0879f278b856ba3a4f881d87499bf5e00877
tayyabhussain03 <tayyabfaisal106@gmail.com>
2025-09-14 19:49:43 +0500

    Component

--- App.jsx
+++ App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-
+import MyButton from './src/components/MyButton'
 export class App extends Component {
   render() {
     return (
-      <View style={{flex:1, justifyContent:"center", alignItems:"center", backgroundColor:"black"}} >
-        <Text style={{fontSize: 32, color:"red", textTransform:"uppercase"}} >tayyab hussain</Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "black" }} >
+        <Text style={{ fontSize: 24, color: "red", textTransform: "uppercase" }} >tayyab hussain</Text>
+        <MyButton />
       </View>
     )
   }
