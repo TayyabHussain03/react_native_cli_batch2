fd926b69591110fd520461b46a976c1dea3e5580
tayyabhussain03 <tayyabfaisal106@gmail.com>
2025-09-18 00:42:02 +0500

    Props relation with child

--- App.jsx
+++ App.jsx
@@ -3,14 +3,16 @@ import { Text, View } from 'react-native'
 import MyButton from './src/components/MyButton'
 import JSX from './src/components/JSX'
 import UseStateHook from './src/components/UseStateHook'
+import PropTest from './src/components/PropTest'
 export class App extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }} >
         {/* <Text style={{ fontSize: 24, color: "red", textTransform: "uppercase" }} >tayyab hussain</Text>
         <MyButton />
-        <JSX /> */}
-        <UseStateHook />
+        <JSX />
+        <UseStateHook /> */}
+        <PropTest/>
       </View>
     )
   }
