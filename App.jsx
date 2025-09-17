5dac18297c11aafc4b18ee56db231dd4d10eb956
tayyabhussain03 <tayyabfaisal106@gmail.com>
2025-09-18 00:15:47 +0500

    UseState Hook

--- App.jsx
+++ App.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import MyButton from './src/components/MyButton'
+import JSX from './src/components/JSX'
+import UseStateHook from './src/components/UseStateHook'
 export class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "black" }} >
-        <Text style={{ fontSize: 24, color: "red", textTransform: "uppercase" }} >tayyab hussain</Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }} >
+        {/* <Text style={{ fontSize: 24, color: "red", textTransform: "uppercase" }} >tayyab hussain</Text>
         <MyButton />
+        <JSX /> */}
+        <UseStateHook />
       </View>
     )
   }
